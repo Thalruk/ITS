75173c0b7fbe9486deefb2c29fd6a7327621a76c
KacperGrodzicki <grodzikg@gmail.com>
2026-04-06 18:55:04 +0200

    Simple shopping cart
    Description, price, quantity and remove function with total price of items

--- my-shop/src/routes/+page.svelte
+++ my-shop/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Home page</h1>
+<a href="/shopping-cart">Shopping Cart</a>
